345edb24fc8c6d1b11b3480efc1f2c966957d3bb
rohitBlueYonder <rohit.rai@jda.com>
2022-10-03 02:27:57 +0530

    Spring_Assignment_1 JDBC and JPA Updated

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(1, 'Ted','Mosby','ted10@gmail.com','123456', null);
-INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(2, 'Lily','Aldrin','lily01@gmail.com','asdfgh', null);
-INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(3, 'Barney','Stinson','barney282@gmail.com','qwerty', null);
-INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(4, 'Ellie','Stinson','Ellie0@gmail.com','anivu', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(1, 'Rohit,'Rai','rohit@gmail.com','123456', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(2, 'Lisa','Thomson','lisa@gmail.com','34563', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(3, 'Mohit','Kapoor','mohit@gmail.com','sgferg', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(4, 'Monika','Roy','Moni@gmail.com','anivu', null);
